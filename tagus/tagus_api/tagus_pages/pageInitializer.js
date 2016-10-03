2cd8fc2fe98b5022b84f6cc87e9a75a00dea06c1
goncalo.assuncao <goncalo.assuncao@enear.co>
2016-10-03 13:40:02 +0100

    changed initializer

--- tagus/tagus_api/tagus_pages/pageInitializer.js
+++ tagus/tagus_api/tagus_pages/pageInitializer.js
@@ -1,87 +1,97 @@
-module.exports = [
-                    {
-                        name: "Home",
-                        url: "/",
-                        createdBy: "user",
-                        created: new Date(),
-                        edited: new Date(),
-                        published: new Date(),
-                        unitType: {
-                            name: "Home",
-                            tabs: [
-                                {
-                                    name: "Content",
-                                    unitFields: [{
-                                        name: "Site Name",
-                                        alias: "siteName",
-                                        type: "text",
-                                        value: "Example Site"
-                                    }]
-                                }
-                            ]
-                        },
-                        template: "index",
-                        parent: "",
-                        isHome: true
+module.exports = [{
+        name: "Home",
+        url: "/",
+        createdBy: "user",
+        created: new Date(),
+        edited: new Date(),
+        published: new Date(),
+        unitType: {
+            name: "Home",
+            tabs: [{
+                name: "Content",
+                unitFields: [{
+                        name: "Site Name",
+                        alias: "siteName",
+                        type: "text",
+                        value: "Example Site"
                     },
                     {
-                        name: "Contacts",
-                        url: "/contacts",
-                        createdBy: "user",
-                        created: new Date(),
-                        edited: new Date(),
-                        published: new Date(),
-                        unitType: {
-                            name: "Home",
-                            unitFields: [{
-                                name: "Site Name",
-                                alias: "siteName",
-                                type: "text",
-                                value: "Example Site"
-                            }]
-                        },
-                        template: "index",
-                        parent: "",
-                        isHome: false
+                        name: "Another Field",
+                        alias: "anotherField",
+                        type: "richText",
+                        value: "This is an example sentence"
                     },
                     {
-                        name: "Emails",
-                        url: "/contacts/emails",
-                        createdBy: "user",
-                        created: new Date(),
-                        edited: new Date(),
-                        published: new Date(),
-                        unitType: {
-                            name: "Home",
-                            unitFields: [{
-                                name: "Site Name",
-                                alias: "siteName",
-                                type: "text",
-                                value: "Example Site"
-                            }]
-                        },
-                        template: "index",
-                        parent: "",
-                        isHome: false
-                    },
-                    {
-                        name: "About",
-                        url: "/about",
-                        createdBy: "user",
-                        created: new Date(),
-                        edited: new Date(),
-                        published: new Date(),
-                        unitType: {
-                            name: "Home",
-                            unitFields: [{
-                                name: "Site Name",
-                                alias: "siteName",
-                                type: "text",
-                                value: "Example Site"
-                            }]
-                        },
-                        template: "index",
-                        parent: "",
-                        isHome: false
+                        name: "True or False",
+                        alias: "trueOrFalse",
+                        type: "boolean",
+                        value: true
                     }
-                ];
+                ]
+            }]
+        },
+        template: "index",
+        parent: "",
+        isHome: true
+    },
+    {
+        name: "Contacts",
+        url: "/contacts",
+        createdBy: "user",
+        created: new Date(),
+        edited: new Date(),
+        published: new Date(),
+        unitType: {
+            name: "Home",
+            unitFields: [{
+                name: "Site Name",
+                alias: "siteName",
+                type: "text",
+                value: "Example Site"
+            }]
+        },
+        template: "index",
+        parent: "",
+        isHome: false
+    },
+    {
+        name: "Emails",
+        url: "/contacts/emails",
+        createdBy: "user",
+        created: new Date(),
+        edited: new Date(),
+        published: new Date(),
+        unitType: {
+            name: "Home",
+            unitFields: [{
+                name: "Site Name",
+                alias: "siteName",
+                type: "text",
+                value: "Example Site"
+            }]
+        },
+        template: "index",
+        parent: "",
+        isHome: false
+    },
+    {
+        name: "About",
+        url: "/about",
+        createdBy: "user",
+        created: new Date(),
+        edited: new Date(),
+        published: new Date(),
+        unitType: {
+            name: "Home",
+            unitFields: [{
+                name: "Site Name",
+                alias: "siteName",
+                type: "text",
+                value: "Example Site"
+            }]
+        },
+        template: "index",
+        parent: "",
+        isHome: false
+    }
+];
--- tagus/tagus_api/tagus_units/unitInitializer.js
+++ tagus/tagus_api/tagus_units/unitInitializer.js
@@ -1,19 +1,26 @@
-module.exports = [
-                    {
-                        name: "Home",
-                        tabs: [
-                            {
-                                name: "Content",
-                                unitFields: [{
-                                    name: "Site Name",
-                                    alias: "siteName",
-                                    type: "text"
-                                }],
-                            }
-                        ],
-                        createdBy: "user",
-                        created: new Date(),
-                        edited: new Date(),
-                        templates: ["index"]
-                    }
-                ];
+module.exports = [{
+    name: "Home",
+    tabs: [{
+        name: "Content",
+        unitFields: [{
+                name: "Site Name",
+                alias: "siteName",
+                type: "text"
+            },
+            {
+                name: "Another Field",
+                alias: "anotherField",
+                type: "richText"
+            },
+            {
+                name: "True or False",
+                alias: "trueOrFalse",
+                type: "boolean"
+            }
+        ]
+    }],
+    createdBy: "user",
+    created: new Date(),
+    edited: new Date(),
+    templates: ["index"]
+}];
