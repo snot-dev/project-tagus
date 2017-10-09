577b360386ff80d9be9cfb4cb7a527cfce492526
snot-dev <snot.dev@gmail.com>
2017-10-09 17:35:30 +0100

    testing units

--- tagus/api/content/tests.js
+++ tagus/api/content/tests.js
@@ -8,6 +8,9 @@ const url = "/api/content/";
 const SharedTests = require("../shared/sharedTests");
 const tests = new SharedTests();
 
+chai.use(chaiHttp);
+mongoose.Promise = require('bluebird');
+
 const mock = {
     _id: new mongoose.mongo.ObjectId('56cb91bdc3464f14678934ca'),
     name: 'testPage',
@@ -29,11 +32,7 @@ const updatedValue = "testUpdate"
 
 const updatedMock = Object.assign(mock, {name: updatedValue});
 
-chai.use(chaiHttp);
-mongoose.Promise = require('bluebird');
-
-
-describe('Content', function(){
+describe('Content', () => {
     it(`Should list all content at ${url} GET`, tests.getAll(url, Content));
     
     it(`Should create a new content in ${url} POST`, tests.createNew(url, Content, mock));
@@ -50,114 +49,4 @@ describe('Content', function(){
     }));
 
     it(`Should delete existing content in ${url}<id> DELETE`, tests.deleteById(url, Content, updatedMock._id));
-
-/*    it("should insert a new page on /api/pages POST", function(done) {
-        var totalPages = 0;
-        chai.request(server)
-        .get('/api/pages')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('array');
-
-            totalPages = res.body.length;
-
-            chai.request(server)
-            .post('/api/pages')
-            .send({
-                name: "Test",
-                url: "/",
-                createdBy: "user",
-                created: new Date(),
-                edited: new Date(),
-                published: new Date(),
-                unitType: {
-                    name: "Test",
-                    unitFields: [{
-                        name: "Test Name",
-                        alias: "test",
-                        type: "text",
-                        value: "This is a test",
-                    }]
-                },
-                template: "index",
-                child: "",
-                parent: ""
-            })
-            .end(function(err, res) {
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.should.be.a('object');
-
-                lib.tests.testRequiredFields(res.body,requiredFields);
-
-                chai.request(server)
-                .get('/api/pages')
-                .end(function(err, res) {
-                    res.should.have.status(200);
-                    res.should.be.json;
-                    res.body.should.be.a('array');
-                    res.body.length.should.to.equal(totalPages + 1);
-                    done();
-                });
-            });
-        });
-    });
-
-    
-
-    it("Should update a single page on /api/pages/<id> POST", function(done) {
-        chai.request(server)
-        .get('/api/pages')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('array');
-
-            var page = res.body[res.body.length - 1];
-            page.name = "This will be deleted afterwards";
-
-            chai.request(server)
-            .post('/api/pages/' + page._id)
-            .send(page)
-            .end(function(err, res) {
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.should.be.a('object');
-                res.body.name.should.to.equal("This will be deleted afterwards");
-
-                done();
-            });
-
-        });
-    });
-
-    it("Should delete a page on api/pages/<id> DELETE", function(done) {
-        var totalPages = 0;
-        var deletedId;
-
-        chai.request(server)
-        .get('/api/pages')
-        .end(function(err, res) {
-            totalPages = res.body.length;
-            deletedId = res.body[totalPages - 1]._id;
-
-            chai.request(server)
-            .delete('/api/pages/' + deletedId)
-            .end(function(err, res) {
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.should.be.a('object');
-                res.body._id.should.to.equal(deletedId);
-
-                chai.request(server)
-                .get('/api/pages')
-                .end(function(err, res) {
-                    res.body.length.should.to.equal(totalPages - 1);
-
-                    done();
-                });
-            });
-        });
-    }); */
 });
--- tagus/api/units/tests.js
+++ tagus/api/units/tests.js
@@ -1,134 +1,55 @@
-var Unit = require('./unitModel');
-var lib = require('../../tagus_lib/lib');
-var requiredFields = lib.models(Unit).getRequiredFields();
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var should = chai.should();
-var server = require('../../../app');
+const Unit = require('./model');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const server = require('../../../app');
+const mongoose = require('mongoose'); 
+const url = "/api/units/";
+const SharedTests = require("../shared/sharedTests");
+const tests = new SharedTests();
 
 chai.use(chaiHttp);
+mongoose.Promise = require('bluebird');
 
-describe('Units', function(){
-    it("Should list all units on /api/units GET", function(done) {
-        chai.request(server)
-        .get('/api/units/')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('array');
-            res.body.forEach(function(unit) {
-                lib.tests.testRequiredFields(unit, requiredFields);
-            });
-            done();
-        });
-    });
+const mock = {
+    _id: new mongoose.mongo.ObjectId('56cb91bdc3464f14678934ca'),
+    name: 'Home',
+    tabs: [
+        {
+            name: 'Content',
+            fields: [
+                {
+                    name: 'Field 1',
+                    alias: 'field1',
+                    type: 'text',
+                    required: true
+                },
+                {
+                    name: 'Field 2',
+                    alias: 'field2',
+                    type: 'richText'
+                }
+            ]
+        }
+    ],
+    createdBy: 'user',
+    created: new Date(),
+    edited: new Date(),
+    templates: ['index']
+};
 
-    it("should insert a new unit on /api/units POST", function(done) {
-        var totalUnits = 0;
-        chai.request(server)
-        .get('/api/units')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('array');
+const updatedValue = "testUpdate"
 
-            totalUnits = res.body.length;
+const updatedMock = Object.assign(mock, {name: updatedValue});
 
-            chai.request(server)
-            .post('/api/units')
-            .send({
-                name: "test",
-                templates: ["test"]
-            })
-            .end(function(err, res) {
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.should.be.a('object');
+describe('Units', () => {
+    it(`Should list all Units at ${url} GET`, tests.getAll(url, Unit));
+    
+    it(`Should create a new Unit in ${url} POST`, tests.createNew(url, Unit, mock));
 
-                lib.tests.testRequiredFields(res.body,requiredFields);
+    it(`Should list a single Unit in ${url}<id> GET`, tests.getOneById(url, Unit, mock._id));
 
-                chai.request(server)
-                .get('/api/units')
-                .end(function(err, res) {
-                    res.should.have.status(200);
-                    res.should.be.json;
-                    res.body.should.be.a('array');
-                    res.body.length.should.to.equal(totalUnits + 1);
-                    done();
-                });
-            });
-        });
-    });
+    it(`Should update existing Unit in ${url}<id> PUT`, tests.updateExisting(url, Unit, updatedMock));
 
-    it("Should list a single unit on /api/units/<id> GET", function(done) {
-        chai.request(server)
-        .get('/api/units/')
-        .end(function(err, res) {
-            chai.request(server)
-            .get("/api/units/" + res.body[0]._id)
-            .end(function(err, res) {
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.should.be.a('object');
-                lib.tests.testRequiredFields(res.body,requiredFields);
-                done();
-            });
-        });
-
-    });
-
-    it("Should update a single unit on /api/units/<id> POST", function(done) {
-        chai.request(server)
-        .get('/api/units')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('array');
-
-            var unit = res.body[res.body.length - 1];
-            unit.name = "This will be deleted afterwards";
-
-            chai.request(server)
-            .post('/api/units/' + unit._id)
-            .send(unit)
-            .end(function(err, res) {
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.should.be.a('object');
-                res.body.name.should.to.equal("This will be deleted afterwards");
-
-                done();
-            });
-
-        });
-    });
-
-    it("Should delete a unit on api/units/<id> DELETE", function(done) {
-        var totalUnits = 0;
-        var deletedId;
-
-        chai.request(server)
-        .get('/api/units')
-        .end(function(err, res) {
-            totalUnits = res.body.length;
-            deletedId = res.body[totalUnits - 1]._id;
-
-            chai.request(server)
-            .delete('/api/units/' + deletedId)
-            .end(function(err, res) {
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.should.be.a('object');
-                res.body._id.should.to.equal(deletedId);
-
-                chai.request(server)
-                .get('/api/units')
-                .end(function(err, res) {
-                    res.body.length.should.to.equal(totalUnits - 1);
-
-                    done();
-                });
-            });
-        });
-    });
+    it(`Should delete existing Unit in ${url}<id> DELETE`, tests.deleteById(url, Unit, updatedMock._id));
 });
