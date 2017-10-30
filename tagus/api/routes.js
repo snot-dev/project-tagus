78be8b739143c980f5696df241006d95b81e1a40
snot-dev <snot.dev@gmail.com>
2017-10-30 11:23:11 +0000

    created site routes and content

--- tagus/api/routes.js
+++ tagus/api/routes.js
@@ -1,15 +1,15 @@
-const router = require('express').Router();
-const content = require('./content/routes');
-const units = require('./units/routes');
-const unitFields = require('./unitFields/routes');
-const users = require('./users/routes');
-const translates = require('./translates/routes');
-const settings = require('./settings/routes');
-const User = require('./users/model');
-const auth = require('./auth/index');
-const bridges = require('./bridges').routes;
+const content = require('./content');
+const units = require('./units').routes;
+const unitFields = require('./unitFields').routes;
+const users = require('./users').routes;
+const translates = require('./translates').routes;
+const bridges = require('./bridges');
+const settings = require('./settings').routes;
+const User = require('./users').model;
+const auth = require('./auth');
 
-const routes = (strategy) => {
+const api = (strategy) => {
+    const router = require('express').Router();
     let protectMiddleware = (req, res, next) => {
         next();
     };
@@ -23,8 +23,8 @@ const routes = (strategy) => {
         protectMiddleware =  auth.passport.authenticate(strategy, session)
     }
     
-    router.use('/content', protectMiddleware, content);
-    router.use('/bridges', protectMiddleware, bridges);
+    router.use('/content', protectMiddleware, content.routes);
+    router.use('/bridges', protectMiddleware, bridges.routes);
     router.use('/units', protectMiddleware, units);
     router.use('/unitfields', protectMiddleware, unitFields);
     router.use('/users', protectMiddleware, users);
@@ -35,5 +35,41 @@ const routes = (strategy) => {
     return router;
 };
 
+const site = () => {
+    const router = require('express').Router();
+    const bridgesContent = {};
 
-module.exports = routes;    
+    bridges.model.find({})
+    .then( docs => {
+        if(docs) {
+            for(let i = 0; i < docs.length; i++) {
+                const bridge = docs[i];
+
+                bridgesContent[bridge.alias] = bridge.content;
+            }
+        }
+
+        return content.model.find({});
+    })
+    .then( docs => {
+        for(let i = 0; i < docs.length; i++ ) {
+            const doc = docs[i];
+            const viewBag = {};
+
+            viewBag[doc.alias] = doc.content;
+            viewBag.bridges = bridgesContent;
+
+            router.get(doc.url, (req, res) => {
+                res.render(doc.template, viewBag);
+            });
+        }
+    });
+
+    return router;
+};
+
+
+module.exports = {
+    api,
+    site
+};
