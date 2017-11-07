4421a1274d577bb5a52464c0887df8bcbede5d82
snot-dev <snot.dev@gmail.com>
2017-11-07 11:52:33 +0000

    removed unused unit

--- tagus/api/units/data.js
+++ tagus/api/units/data.js
@@ -45,55 +45,6 @@ module.exports = [
         ]
     },
     {
-        _id: "5a017b0983dd7214c8661649",
-        name: "Banner",
-        alias: "banner",
-        templates: [
-            "master",
-            "home"
-        ],
-        edited: new Date(),
-        created: new Date(),
-        tabs: [
-            {
-                name: "Default",
-                alias: "default",
-                fields: [
-                    {
-                        name: "Title",
-                        alias: "title",
-                        type: "text",
-                        required: true
-                    },
-                    {
-                        name: "Sub Title",
-                        alias: "subTitle",
-                        type: "text",
-                        required: true
-                    },
-                    {
-                        name: "Link",
-                        alias: "link",
-                        type: "text",
-                        required: false
-                    },
-                    {
-                        name: "Link Text",
-                        alias: "linkText",
-                        type: "text",
-                        required: false
-                    },
-                    {
-                        name: "Banner Image",
-                        alias: "bannerImg",
-                        type: "text",
-                        required: false
-                    }
-                ]
-            }
-        ]
-    },
-    {
         _id: "5a017b9883dd7214c866164a",
         name: "Icon",
         alias: "icon",
