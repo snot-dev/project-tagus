58dcfc396d7799a9b7823ff8009b11c273995d45
snot-dev <snot.dev@gmail.com>
2017-11-08 18:06:57 +0000

    created components folder

--- tagus/client/src/components/App/index.js
+++ tagus/client/src/components/App/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 class App extends Component {
   render() {
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, hellloooo! edit <code>src/App.js</code> and save to reload.
+          To get started, ah hoy! edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
