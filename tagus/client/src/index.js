97def74be3b7b55cebd3bff2185ecbf26eef91f6
snot-dev <snot.dev@gmail.com>
2017-11-09 17:45:09 +0000

    scaffolded with components. Will try scenes approach

--- tagus/client/src/index.js
+++ tagus/client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './vendor/bootstrap/css/bootstrap.min.css';
+import './vendor/bootstrap/css/bootstrap-theme.min.css';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
