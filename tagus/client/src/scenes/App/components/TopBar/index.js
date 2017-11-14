f555ebd4c3d78ca556efcdb81ef7561675b7cc8d
snot-dev <snot.dev@gmail.com>
2017-11-14 16:57:52 +0000

    top bar

---
+++ tagus/client/src/scenes/App/components/TopBar/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './topBar.css';
+
+class TopBar extends Component {
+    render() {
+        return (
+            <div id="top-bar">
+
+            </div>
+        );
+    }
+}
+
+export default TopBar;
--- tagus/client/src/scenes/App/index.js
+++ tagus/client/src/scenes/App/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AppBar from './components/AppBar';
+import TopBar from './components/TopBar';
 import logo from './logo.svg';
 import style from './style.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div id ="tagus-app" className="App">
+        <TopBar />
         <AppBar />
       </div>
     );
