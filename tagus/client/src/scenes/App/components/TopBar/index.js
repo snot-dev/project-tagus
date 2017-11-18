4d6f90a418944dc6a3c76ba97eda2b45fadfaaa5
snot-dev <snot.dev@gmail.com>
2017-11-18 01:55:56 +0000

    added a title to the top bar

--- tagus/client/src/scenes/App/components/TopBar/index.js
+++ tagus/client/src/scenes/App/components/TopBar/index.js
@@ -4,9 +4,15 @@ import './topBar.css';
 class TopBar extends Component {
     render() {
         return (
-            <div id="top-bar">
-
-            </div>
+            <nav id="top-bar">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div id="navHeaderContainer" className="col-xs-6">
+                            <h1 id="navHeader">tagus</h1>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         );
     }
 }
