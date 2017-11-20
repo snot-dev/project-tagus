d1afc6b474bb162e6885e5d41a480fc60b5e6f22
snot-dev <snot.dev@gmail.com>
2017-11-20 15:20:49 +0000

    connected app with redux

--- tagus/client/src/index.js
+++ tagus/client/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HashRouter, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import './vendor/bootstrap/css/bootstrap.min.css';
 import './vendor/bootstrap/css/bootstrap-theme.min.css';
 import './vendor/font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './scenes/App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './services/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <HashRouter >
+            <Route path="/" component={App} >
+        
+            </Route>
+        </HashRouter>
+    </Provider>,
+    document.getElementById('root')
+    );
+    
 registerServiceWorker();
