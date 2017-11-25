fb0066fe2e8d6e559124f481509c74cd4deade0b
Snot <snot.dev@gmail.com>
2017-11-25 10:21:56 +0000

    populating with mongoose

--- tagus/api/content/routes.js
+++ tagus/api/content/routes.js
@@ -1,4 +1,14 @@
 const Content = require('./model');
-const router = require('../router/router');
+//const router = require('../router/router');
+const router = require('express').Router();
 
-module.exports = router.defineCRUDRoutes(Content);
+router.get('/', (req, res) => {
+    Content.find({})
+    .populate('children')
+    .populate('unitType')
+    .exec((err,docs) => {
+        res.json(err || docs);
+    });  
+});
+
+module.exports = router;
