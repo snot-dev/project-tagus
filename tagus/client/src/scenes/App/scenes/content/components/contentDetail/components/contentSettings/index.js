9818ec9e058c67ba11bc785e97d9a1b0b5e04457
snot-dev <snot.dev@gmail.com>
2017-11-29 17:56:29 +0000

    settings files

---
+++ tagus/client/src/scenes/App/scenes/content/components/contentDetail/components/contentSettings/index.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import {Form, StyledText, Checkbox } from 'react-form';
+import {Button} from 'react-bootstrap';
+import CancelModal from '../cancelModal';
+
+class ContentSettings extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            cancelMode: false,
+            formWasTouched: false
+        }
+    }
+
+    _getFieldType(field) {
+        switch(field.type) {
+            case'text':
+                return StyledText;
+            case 'checkbox':
+                return Checkbox;
+            default:
+                break;
+        }
+    }
+
+    _renderField(field) {
+        const Field = this._getFieldType(field);
+
+        return (
+            <div className="col-xs-12 tagus-form-field">
+                <label className="tagus-label" htmlFor={field.alias}>{field.name}</label>
+                <Field onChange={this._touchTheForm.bind(this)} className={`tagus-input ${field.type}`}   field={field.alias} id={field.alias} />                
+            </div>
+        )
+    }
+
+    _errorValidator(values) {
+        const errors = {};
+
+        for(const field of this.props.fields) {
+            errors[field.alias] = field.required && values[field.alias] === ''?"This field is required!":null;
+        }
+
+
+        return errors;
+    }
+
+    //TODO: Only send if form was touched
+    _onSubmit(formApi) {
+        return () => {
+            if(this.state.formWasTouched){
+                this.props.detail.content[this.props.name] = formApi.values;
+                formApi.submitForm();
+                this.props.onSubmit(this.props.detail);
+            }
+        }
+    }
+
+    _resetForm(formApi) {
+        return () => {
+            this.setState({cancelMode: false});
+            formApi.resetAll();
+        }
+    }
+
+    _toggleCancelModal(show) {
+        return() => {
+            if(this.state.formWasTouched) {
+                this.setState({cancelMode: show});
+            }
+        }
+    }
+
+    _touchTheForm() {
+        if(!this.state.formWasTouched){
+            this.setState({formWasTouched: true});
+        }
+    }
+
+    render() {
+        const disabled = this.state.formWasTouched ? "" : "disabled";
+        return (
+            <Form dontValidateOnMount={true} validateError={this._errorValidator.bind(this)} defaultValues={this.props.defaultValues}>
+                {formApi => (
+                    <form onSubmit={formApi.submitForm} className="container-fluid">
+                        {this.props.fields.map((field, fieldIndex) => (
+                            <div className="row tagus-form-control" key={field.alias+fieldIndex}>
+                                {this._renderField(field, formApi)}
+                            </div>
+                        ))}
+                        <div className="row">
+                            <div className="tagus-form-button-container col-xs-12">
+                                <Button onClick={this._onSubmit(formApi).bind(this)} type="button" className={`pull-right ${disabled}`} bsStyle={"primary"}>Save</Button>
+                                <Button onClick={this._toggleCancelModal(true).bind(this)} className={`pull-left ${disabled}`}>Cancel</Button>
+                            </div>
+                        </div>
+                    <CancelModal show={this.state.cancelMode} confirmButton={this._resetForm(formApi)}  closeButton={this._toggleCancelModal(false)} />
+                    </form>
+                )}
+            </Form>
+        );
+    };
+}
+
+export default ContentSettings;
