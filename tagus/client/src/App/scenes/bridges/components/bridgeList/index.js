d89a7096a80b453a4dc9ab1f339ba5e5312c7cdf
Snot <snot.dev@gmail.com>
2018-02-13 19:22:14 +0000

    showing modal

--- tagus/client/src/App/scenes/bridges/components/bridgeList/index.js
+++ tagus/client/src/App/scenes/bridges/components/bridgeList/index.js
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom';
 import List from '../../../../components/List';
 import Panel from '../../../../components/Panel';
 import ListItem from '../../../../components/ListItem';
-import Modal from '../../../../components/Modal ';
+import Modal from '../../../../components/Modal';
 import AddLink from '../../../../components/AddLink';
 import CreateBridgeMenu from './components/createBridgeMenu';
 import Overlay from '../../../../components/Overlay';
@@ -22,9 +22,11 @@ class BridgeList extends Component {
     }
 
     toggleDeleteModal(show) {
-        this.setState({
-            deleteMode: show
-        });
+        return() => {
+            this.setState({
+                deleteMode: show
+            });
+        };
     }
 
     toggleCreatingBridge(state) {
@@ -33,6 +35,12 @@ class BridgeList extends Component {
                 creatingBridge: state
             });
         }
+    }
+
+    _deleteBridge() {
+        this.setState({
+            deleteMode: false
+        });
     }
 
     onSubmitCreatingBridge(values) {
@@ -58,7 +66,7 @@ class BridgeList extends Component {
                                 <ListItem key={`${bridge._id}_${key}`} className="tagus-bridges-list-item">
                                     <NavLink to={`${this.props.url}/detail/${bridge._id}`} activeClassName="active" className="tagus-list-item-link">
                                         <i className={`fa fa-file`} aria-hidden="true"></i>{bridge.name}
-                                        <div className="tagus-bridges-list-delete">
+                                        <div onClick={this.toggleDeleteModal(bridge._id)} className="tagus-bridges-list-delete">
                                             <i className="fa fa-trash-o" aria-hidden="true"></i>
                                         </div>
                                     </NavLink>
@@ -69,7 +77,7 @@ class BridgeList extends Component {
                 </List>
                 <AddLink text="Create new Bridge" disabled={this.state.creatingBridge} onClick={this.toggleCreatingBridge(true)} />
                 <Overlay show={this.props.savingDetail || this.props.fetchingList || this.props.fetchingDetail}/>
-                <Modal show={this.state.deleteMode} title="Warning!" body={"Are you sure you want to DELETE PERMANENTLY this page and all the children?"} closeButton={{onClick: this._toggleModal(false), text: "Cancel"}} confirmButton={{onClick:this._deleteContent.bind(this), text: "Yes, I'm sure!"}} />
+                <Modal show={!!this.state.deleteMode} title="Warning!" body={"Are you sure you want to DELETE PERMANENTLY this bridge?"} closeButton={{onClick: this.toggleDeleteModal(false), text: "Cancel"}} confirmButton={{onClick:this._deleteBridge.bind(this), text: "Yes, I'm sure!"}} />
             </Panel>
         );
     }
