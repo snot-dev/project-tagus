bc0e4311660f40cbd697b35af34c5c3fb34eaacd
Goncalo Assuncao <ext.goncalo.assuncao@ingg.com>
2018-02-22 11:38:47 +0000

    using async loop to build DB

--- tagus/api/buildDB/initScript.js
+++ tagus/api/buildDB/initScript.js
@@ -1,3 +1,4 @@
+const async = require('async');
 const mongoose = require('mongoose');
 const usersSeed = require('../users/data');
 const User = require('../users').model;
@@ -21,108 +22,69 @@ let collectionCreated = false;
 
 console.log("Connecting to " + process.env.MONGO_CONNECTION_STRING)
 
+const collections = [
+    {
+        model: Settings,
+        seed: settingsSeed
+    },
+    {
+        model: User,
+        seed: usersSeed
+    },
+    {
+        model: Translate,
+        seed: translatesSeed    
+    },
+    {
+        model: UnitField,
+        seed: unitFieldsSeed    
+    },
+    {
+        model: Unit,
+        seed: unitsSeed    
+    },
+    {
+        model: Bridges,
+        seed: bridgesSeed    
+    },
+    {
+        model: Content,
+        seed: contentSeed    
+    }
+];
 
-//TODO: Refactor this to a loop!
+
+function insertCollection(collection, done) {
+     collection.model.find({})
+    .then(docs => {
+        if (docs.length === 0) {
+            return collection.model.insertMany(collection.seed);
+        }
+        else {
+            collectionCreated = true;
+        }
+    })
+    .then(() => {
+        const collectionName = collection.model.collection.collectionName;
+        console.log(collectionCreated ? `${collectionName} already created!` : `Inserted ${collectionName}`);
+        done();
+    })
+    .catch( err => {
+        console.log(err);
+        process.exit(1);
+        done();
+    });
+}
+
 mongoose.connect(process.env.MONGO_CONNECTION_STRING)
 .then(() => {
 
     console.log("Connected! Building DB....");
 
-    Settings.find({}).then(settings => {
-        if( settings.length === 0) {
-            return Settings.insertMany(settingsSeed)
-        }
-        else {
-            collectionCreated = true;
-        }
-    })
-    .then(settings => {
-        console.log(collectionCreated ? 'Settings already created!': 'Inserted Settings!' );
-        collectionCreated = false;
-        return User.find({});
-    })
-    .then( users => {
-        if(users.length === 0) {
-            return User.insertMany(usersSeed);
-        } 
-        else {
-            collectionCreated = true;
-        }
-    })
-    .then(settings => {
-        console.log(collectionCreated ? 'Users already created!': 'Inserted Users!' );
-        collectionCreated = false;
-        return Translate.find({});
-    })
-    .then(translates => {
-        if(translates.length === 0) {
-            return Translate.insertMany(translatesSeed);
-        } 
-        else {
-            collectionCreated = true;
-        }
-    })
-    .then(translates => {
-        console.log(collectionCreated ? 'Translates already created!' : 'Inserted Translates!');
-        collectionCreated = false;
-        return UnitField.find({});
-    })
-    .then(unitFields => {
-        if(unitFields.length === 0) {
-            return UnitField.insertMany(unitFieldsSeed);
-        } else {
-            collectionCreated = true;
-        }
-    })
-    .then(unitFields => {
-        console.log(collectionCreated ? 'UnitFields already created!' : 'Inserted UnitFields!!');
-        collectionCreated = false;
-        return Unit.find({});
-    })
-    .then(units => {
-        if(units.length === 0) {
-            return Unit.insertMany(unitsSeed);
-        } 
-        else {
-            collectionCreated = true;
-            return units;
-        }
-    })
-    .then(units => {
-        console.log(collectionCreated ? 'Units already created!' : 'Inserted Units!');
-        collectionCreated = false;
-        return Bridges.find({});
-    })
-    .then( bridges => {
-        if(bridges.length === 0) {
-            Bridges.insertMany(bridgesSeed);
-        } 
-        else {
-            collectionCreated = true;
-        }
-    })
-    .then(bridges => {
-        console.log(collectionCreated ? 'Bridges already created!' : 'Inserted Bridges!');
-        collectionCreated = false;
-        return Content.find({});
-    })
-    .then(content => {
-        if(content.length === 0) {
-            return Content.insertMany(contentSeed);
-        } 
-        else {
-            collectionCreated = true;
-            return content;
-        }
-    })
-    .then(content => {
-        console.log(collectionCreated ? 'Content already created!' : 'Inserted Content!');
-        collectionCreated = false;
+    async.eachSeries(collections, (collection, done) => {
+        insertCollection(collection, done);
+    }, (err) => {
         console.log("DB was built!");
         process.exit(0);
-    })
-    .catch(err => {
-        console.log(err);
-        process.exit(1);
     });
 });
