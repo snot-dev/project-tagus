920f6fb9b6cd139a894b9e9809300f8481c26a74
Goncalo Assuncao <ext.goncalo.assuncao@ingg.com>
2018-04-10 10:28:40 +0100

    Running the update test

--- tagus/api/tests/bridges.js
+++ tagus/api/tests/bridges.js
@@ -1,16 +1,16 @@
 const Bridge = require('../bridges/model');
 const chai = require('chai');
-const chaiHttp = require('chai-http');
-const should = chai.should();
-const mongoose = require('mongoose'); 
+// const chaiHttp = require('chai-http');
+// const should = chai.should();
+// const mongoose = require('mongoose'); 
 const SharedTests = require("../shared/tests");
 const tests = new SharedTests();
 
 const testName = "Bridges";
 const url = "/tagus/api/bridges/";
 
-chai.use(chaiHttp);
-mongoose.Promise = require('bluebird');
+// chai.use(chaiHttp);
+// mongoose.Promise = require('bluebird');
 
 const mock = {
     _id: new mongoose.mongo.ObjectId('56cb91bdc3464f14678934cb'),
