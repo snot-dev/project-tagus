3d71720a3a99ddf64fbfaae00937c82a3c194ae3
Goncalo Assuncao <ext.goncalo.assuncao@ingg.com>
2018-04-12 14:06:09 +0100

    responsive top bar

--- tagus/client/src/App/components/TopBar/index.js
+++ tagus/client/src/App/components/TopBar/index.js
@@ -9,11 +9,11 @@ class TopBar extends Component {
             <nav id="tagus-top-bar">
                 <div id="tagus-nav-header-container">
                     <Link to="/home" id="tagus-nav-logo">
-                        <img src="/logo/Tagus_Logo_Horizontal_White_Smaller.png" title="tagus" alt="tagus" />
+                        <img className="tagus-logo" src="/logo/Tagus_Logo_Horizontal_White_Smaller.png" title="tagus" alt="tagus" />
                     </Link>
                 </div>
                 <div className="tagus-top-bar-container">
-                    <div className="pull-right tagus-top-buttons-container">
+                    <div className="tagus-top-buttons-container">
                          <NavLink to='/home/profile' className="tagus-top-avatar tagus-top-button" title="profile" ><i className="fa fa-user"></i><span className="tagus-top-user-username">{this.props.user.username}</span></NavLink>
                          <a onClick={this.props.onLogoffClick} className="tagus-logoff-button tagus-top-button" title="log off"><i className="fa fa-power-off"></i></a>
                     </div>
