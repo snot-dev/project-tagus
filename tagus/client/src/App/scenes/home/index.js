c59f155c112f3a304892c6d4c45ba28c7371fabf
Goncalo Assuncao <ext.goncalo.assuncao@ingg.com>
2018-04-13 16:51:31 +0100

    added panel

--- tagus/client/src/App/scenes/home/index.js
+++ tagus/client/src/App/scenes/home/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
+import Panel from '../../components/Panel';
 
 
 class Home extends Component {
     render() {
         return (
-            <div>HOME SCREEN WILL APPEAR HERE</div>
+            <section id="home" className="full-height col-xs-12">
+            <Panel className="col-xs-12 full-height">
+                HOME SCREEN WILL APPEAR HERE
+            </Panel>
+            </section>
         );
     }
 }
